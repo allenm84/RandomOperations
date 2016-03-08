d1dd9e355649968cd23c6197621f64d223523111
Michael Allen <michael.allen@cognex.com>
2016-03-08 12:29:39 -0600

    Added Text->Binary and Binary->Text operations

---
+++ src/RandomOperations/Operations/BinaryToText.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomOperations
+{
+  public class BinaryToText : Operation
+  {
+    private TextInput input;
+
+    public BinaryToText()
+    {
+      input = new TextInput
+      {
+        Text = string.Empty,
+      };
+    }
+
+    public override string DisplayName
+    {
+      get { return "Binary to Text"; }
+    }
+
+    public override bool NeedsInput
+    {
+      get { return true; }
+    }
+
+    public override object DataSource
+    {
+      get { return input; }
+    }
+
+    public override string[] GetResult()
+    {
+      return new[]
+      {
+        new string(input.Text.Split(' ')
+          .Select(b => Convert.ToInt32(b, 2))
+          .Select(Convert.ToChar)
+          .ToArray()),
+      };
+    }
+  }
+}
---
+++ src/RandomOperations/Operations/TextToBinary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomOperations
+{
+  public class TextToBinary : Operation
+  {
+    private TextInput input;
+
+    public TextToBinary()
+    {
+      input = new TextInput
+      {
+        Text = string.Empty,
+      };
+    }
+
+    public override string DisplayName
+    {
+      get { return "Text to Binary"; }
+    }
+
+    public override bool NeedsInput
+    {
+      get { return true; }
+    }
+
+    public override object DataSource
+    {
+      get { return input; }
+    }
+
+    public override string[] GetResult()
+    {
+      return new[]
+      {
+        string.Join(" ", input.Text
+          .Select(Convert.ToInt32)
+          .Select(i => Convert.ToString(i, 2).PadLeft(8, '0'))),
+      };
+    }
+  }
+}
